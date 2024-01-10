fad542af10a1bad70822e953b3696631f00c63d0
Aldy Rahman <aldy.rahman02@gmail.com>
2024-01-10 13:54:18 +0800

    Style: Change Navigation Bar Size

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,21 +9,21 @@ const Navigation = () => {
         <div className="navbar flex flex-row justify-between px-8 py-2 border-b-2 border[#B0BEC5]">
             <div className='flex flex-row items-center'>
                 <div className="logo-container">
-                    <img src={Logo} alt="Trash2Cash Logo" className='w-56' />
+                    <img src={Logo} alt="Trash2Cash Logo" className='w-44' />
                 </div>
-                <span className="border-l-[3px] h-12 border-[#DEDEDE] ml-5 mr-6"></span>
-                <div className="item-menu font-medium text-2xl flex flex-row items-center text-[#455A64]">
+                <span className="border-l-[3px] h-8 border-[#DEDEDE] ml-5 mr-6"></span>
+                <div className="item-menu font-medium text-xl flex flex-row items-center text-[#455A64]">
                     <p>Homapage</p>
                     <p className='mx-4'>Trash Pickup</p>
                     <p>Product</p>
                 </div>
             </div>
             <div className="profile flex flex-row items-center">
-                <img src={CartIcon} alt="Shopping Cart Icon" />
-                <img src={NotificationIcon} alt="Notification Icon" className='ml-5' />
-                <span className="border-l-[3px] h-12 border-[#DEDEDE] mx-5"></span>
-                <img src={ProfiileIcon} alt="Profile Icon" />
-                <img src={ArrowIcon} alt="Arrow Icon" className='ml-5' />
+                <img className='w-7' src={CartIcon} alt="Shopping Cart Icon" />
+                <img src={NotificationIcon} alt="Notification Icon" className='ml-5 w-7' />
+                <span className="border-l-[3px] h-8 border-[#DEDEDE] mx-5"></span>
+                <img className='w-8' src={ProfiileIcon} alt="Profile Icon" />
+                <img src={ArrowIcon} alt="Arrow Icon" className='ml-2 w-6' />
             </div>
         </div>
     );
