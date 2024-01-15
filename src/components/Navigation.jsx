0e90ebb4abdbc2c8f7f0a40accd5e653d2695fe0
Aldy Rahman <aldy.rahman02@gmail.com>
2024-01-15 18:10:20 +0800

    Style: Make TrashPickup Page Responsive For Mobile Device

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,26 +7,26 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <div className="navbar flex flex-row justify-between px-8 py-2 border-b-2 border[#B0BEC5]">
+        <div className="navbar flex flex-row justify-between px-6 xl:px-8 py-2 border-b-2 border[#B0BEC5]">
             <div className='flex flex-row items-center'>
                 <Link to='/'>
                 <div className="logo-container">
-                    <img src={Logo} alt="Trash2Cash Logo" className='w-44' />
+                    <img src={Logo} alt="Trash2Cash Logo" className='w-32 xl:w-44' />
                 </div>
                 </Link>
-                <span className="border-l-[3px] h-8 border-[#DEDEDE] ml-5 mr-6"></span>
-                <div className="item-menu font-medium text-xl flex flex-row items-center text-[#455A64]">
+                <span className="hidden xl:block xl:border-l-[3px] xl:h-8 xl:border-[#DEDEDE] xl:ml-5 xl:mr-6"></span>
+                <div className="text-[#455A64] items-center hidden xl:flex xl:flex-row xl:font-medium xl:text-xl">
                     <Link to='/'><p className='hover:text-black'>Homepage</p></Link>
                     <Link to='/pickup'><p className='mx-4 hover:text-black'>Trash Pickup</p></Link>
                     <Link to='/product'><p className='hover:text-black'>Product</p></Link>
                 </div>
             </div>
             <div className="profile flex flex-row items-center">
-                <img className='w-7' src={CartIcon} alt="Shopping Cart Icon" />
-                <img src={NotificationIcon} alt="Notification Icon" className='ml-5 w-7' />
-                <span className="border-l-[3px] h-8 border-[#DEDEDE] mx-5"></span>
-                <img className='w-8' src={ProfiileIcon} alt="Profile Icon" />
-                <img src={ArrowIcon} alt="Arrow Icon" className='ml-2 w-6' />
+                <img className='w-5 xl:w-7' src={CartIcon} alt="Shopping Cart Icon" />
+                <img src={NotificationIcon} alt="Notification Icon" className='w-5 ml-3 xl:w-7 xl:ml-5' />
+                <span className="border-l-2 border-[#DEDEDE] h-6 mx-3 xl:mx-5 xl:border-l-[3px] xl:h-8"></span>
+                <img className='w-6 xl:w-8' src={ProfiileIcon} alt="Profile Icon" />
+                <img src={ArrowIcon} alt="Arrow Icon" className='ml-1 w-4 xl:ml-2 xl:w-6' />
             </div>
         </div>
     );
--- src/pages/TrashPickup.jsx
+++ src/pages/TrashPickup.jsx
@@ -43,16 +43,16 @@ const TrashPickup = () => {
 
   return (
     <div>
-      <div className="px-10">
-        <h1 className="animation2 font-bold text-2xl pt-6 pb-4">Mari Mengolah Sampah Menjadi Cuan Bersama Kami</h1>
-        <iframe className='animation2 h-64 w-full'
+      <div className="px-7 xl:px-10">
+        <h1 className="animation2 font-semibold text-base py-6 xl:font-bold xl:text-2xl xl:pt-6 xl:pb-4">Mari Mengolah Sampah Menjadi Cuan Bersama Kami</h1>
+        <iframe className='animation2  w-full h-48 xl:h-64'
           title="Current Location"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.8444981505824!2d116.83418621432783!3d-1.2622993359491928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x323623e18f13e041%3A0x1d9ef4cdef6da70e!2sBSB%20Mall!5e0!3m2!1sen!2sid!4v1643288365965!5m2!1sen!2sid"
           allowFullScreen
         ></iframe>
 
         {/* Stepper Started */}
-        <div className='my-6 animation2'>
+        <div className='hidden xl:visible xl:my-6 animation2'>
           <Stepper className='animation2' index={activeStep} colorScheme='teal'>
             {steps.map((step, index) => (
               <Step key={index}>
@@ -76,12 +76,12 @@ const TrashPickup = () => {
         {/* Stepper End */}
 
         {/* Input Search */}
-        <div className='animation2 flex items-center justify-center flex-col w-full mt-[58px]'>
-          <div className="relative w-2/3">
+        <div className='animation2  w-full flex items-center justify-center flex-col mt-9 xl:mt-[58px]'>
+          <div className="relative w-full xl:w-2/3">
             <input
               type="text"
               placeholder="Choose your nearest trash bank"
-              className='w-full py-2 pl-11 pr-5 rounded-full border-2 border-gray-400'
+              className='text-sm xl:text-base w-full py-2 pl-9 xl:pl-11 pr-5 rounded-full border-[1px] xl:border-2 border-gray-500'
             />
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <svg
@@ -92,23 +92,23 @@ const TrashPickup = () => {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="h-6 w-6 text-gray-400"
+                className="h-4 w-4 xl:h-6 xl:w-6 text-gray-400"
               >
                 <circle cx="11" cy="11" r="8"></circle>
                 <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
               </svg>
             </div>
           </div>
-          <div className='sorting-container w-2/3 mt-6'>
-            <span className='border-[1px] border-[#DCFFDC] bg-[#DCFFDC] text-[#65B741] py-2 px-4 rounded-xl ml-3'>Open</span>
-            <span className='border-[1px] border-[#7B7B7B] text-[#7B7B7B]  py-2 px-4 rounded-xl ml-3'>Nerby</span>
-            <span className='border-[1px] border-[#7B7B7B] text-[#7B7B7B]  py-2 px-4 rounded-xl ml-3'>Popular</span>
-            <span className='border-[1px] border-[#7B7B7B] text-[#7B7B7B]  py-2 px-4 rounded-xl ml-3'>All Banks</span>
+          <div className='w-full xl:w-2/3 mt-3 xl:mt-6'>
+            <span className='border-[1px] border-[#DCFFDC] bg-[#DCFFDC] text-[#65B741] py-1 xl:py-2 px-3 xl:px-4 xl:ml-3 ml-1 rounded-lg xl:rounded-xl text-xs xl:text-base'>Open</span>
+            <span className='border-[1px] border-[#7B7B7B] text-[#7B7B7B] py-1 xl:py-2 px-3 xl:px-4 rounded-lg xl:rounded-xl ml-2 xl:ml-3 text-xs xl:text-base'>Nerby</span>
+            <span className='border-[1px] border-[#7B7B7B] text-[#7B7B7B] py-1 xl:py-2 px-3 xl:px-4 rounded-lg xl:rounded-xl ml-2 xl:ml-3 text-xs xl:text-base'>Popular</span>
+            <span className='border-[1px] border-[#7B7B7B] text-[#7B7B7B] py-1 xl:py-2 px-3 xl:px-4 rounded-lg xl:rounded-xl ml-2 xl:ml-3 text-xs xl:text-base'>All Banks</span>
           </div>
         </div>
 
         {/* Bank List Started */}
-        <div className='w-full grid grid-cols-3 gap-y-8 gap-x-9 mt-16'>
+        <div className='w-full grid xl:grid-cols-3 grid-cols-1 gap-y-6 xl:gap-y-8 xl:gap-x-9 mt-9 xl:mt-16'>
           {trashBankDatas.map((trashBankData) => (
             <BankCard key={trashBankData.id}
               bankName={trashBankData.title}
