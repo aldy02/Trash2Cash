8778d1403934bc5645af0a9eb64877053f0a0ee2
Aldy Rahman <aldy.rahman02@gmail.com>
2024-01-17 21:19:22 +0800

    Style: Update SignIn and SignUp Responsive

--- src/components/AuthCard.jsx
+++ src/components/AuthCard.jsx
@@ -8,10 +8,10 @@ const AuthCard = ({ children, authType }) => {
     nextPage: isLogin ? 'Sign Up' : 'Sign In',
   };
   return (
-    <div className="auth-container h-screen flex flex-col justify-center">
-      <div className="px-8 py-4 flex flex-col justify-center content-center h-fit rounded-md shadow-md w-96 border border-neutral-500">
-        <h1 className="font-bold text-center text-3xl">{authContent.header}</h1>
-        <div className="flex w-fit mx-auto mb-4">
+    <div className="auth-container h-screen flex flex-col justify-center px-5">
+      <div className="lg:px-8 px-6 py-4 flex flex-col justify-center content-center h-fit rounded-md shadow-md w-full border border-neutral-500">
+        <h1 className="font-bold text-center lg:text-3xl text-2xl">{authContent.header}</h1>
+        <div className="flex w-fit mx-auto mb-4 text-sm lg:text-base">
           <p className="text-center">{authContent.subHeader}</p>
           <a
             className="ml-2 text-[#65B741] font-medium"
