397003cd87c38caaf9f1905efd69b789bb4f2208
Aldy Rahman <aldy.rahman02@gmail.com>
2024-01-18 15:15:34 +0800

    Style: Update SignIn

--- src/components/AuthCard.jsx
+++ src/components/AuthCard.jsx
@@ -9,7 +9,7 @@ const AuthCard = ({ children, authType }) => {
   };
   return (
     <div className="auth-container h-screen flex flex-col justify-center px-5">
-      <div className="lg:px-8 px-6 py-4 flex flex-col justify-center content-center h-fit rounded-md shadow-md w-full border border-neutral-500">
+      <div className="lg:px-8 px-6 py-4 flex flex-col justify-center content-center h-fit rounded-md shadow-md max-w-96 border border-neutral-500">
         <h1 className="font-bold text-center lg:text-3xl text-2xl">{authContent.header}</h1>
         <div className="flex w-fit mx-auto mb-4 text-sm lg:text-base">
           <p className="text-center">{authContent.subHeader}</p>
